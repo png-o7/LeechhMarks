199b2c11ba5251a30a856e123b2da7e1a26d08c6
png <maycleor.contact@gmail.com>
2026-06-01 16:41:58 +0200

    Add files via upload

---
+++ LeechsMarks.lua
@@ -0,0 +1,8 @@
+## Interface: 120005
+## Title: LeechsMarks
+## Notes: Auto-marks enemies in M+, marks tanks and healers
+## Version: 1.4.0
+## Author: Leech
+## SavedVariables: LeechsMarksDB
+
+LeechsMarks.lua
